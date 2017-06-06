63f83516aeaa3a08fc4df5744edd273f7d5b61a6
James Slattery <james@slattery.tech>
2017-06-06 11:13:48 -0700

    Aggregate

--- server/publish.js
+++ server/publish.js
@@ -1,15 +1,18 @@
 Meteor.publish('feedback', function () {
       return feedback.find({});
 });
-Meteor.publish('feedBackOverall', function(){
-  var connect = ({
-    "$group": {
-      "_id": "$overallScore",
-      "avgScore": { "$avg": "$overallScore" }
+Meteor.publish('feedBackOverall', function(args){
+  var connect = [
+    {
+      "$group": {
+        "_id": "$overallScore",
+        "avgScore": { "$avg": "$overallScore" }
+      }
     }
-  });
+  ];
 
-  var result = feedback.aggregate(connect);
-  console.log(result);
+  var result = feedback.aggregate(connect)
+  
+
   return result;
 });
